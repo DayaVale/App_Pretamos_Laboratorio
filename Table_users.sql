491a6013c1ca2f138c0df6a5ccc0911fc76b3023
Dayana Gonzalez <56181593+DayaVale@users.noreply.github.com>
2023-03-13 18:35:22 -0500

    Avances Usuarios
    Creacion de la conexion y la tabla usuarios

---
+++ Table_users.sql
@@ -0,0 +1,38 @@
+--------- CREATE TABLE USERS -----------------
+--- Primary key ID 
+--- Vachar Name 
+--- Vachar LastName
+--- Vachar Email
+--- Vachar(8-10) Password
+--- Vachar Phone
+--- Vachar Carrer
+--- Vachar Check(Student,Professor,Admin) Role
+----------------------------------------------
+
+CREATE TABLE Usuarios (
+	ID_Usuario bigint NOT NULL PRIMARY KEY,
+	PrimerNombre varchar(255) NOT NULL,
+	SegundoNombre varchar(255),
+	PrimerApellido varchar(255) NOT NULL,
+	SegundoApellido varchar(255),
+	Email varchar(255) NOT NULL UNIQUE,
+	Contrasena varchar(30) NOT NULL CHECK(CHAR_LENGTH(Contrasena) = 8),
+	Celular bigint,
+	Programa varchar(255) NOT NULL,
+	Rol varchar(255) NOT NULL CHECK(Rol = 'estudiante'OR Rol = 'profesor'
+													  OR Rol = 'Admin')
+);
+
+---------------------------------------------------------------------------
+SELECT * FROM usuarios;
+
+---------------------------------------------------------------------------
+
+INSERT INTO usuarios
+VALUES (1000591286,'dayana','valentina',
+		'gonzalez','vargas','dayana.gonzalez@urosario.edu.co',
+		'candy123',3113305548,'Macc','estudiante');
+
+---------------------------------------------------------------------------
+
+DROP TABLE usuarios;
